43459687983f4a9618112d9c5dbde6df1a755d54
Filip Jankoski <filip@agmedia.hr>
2024-03-11 20:16:43 +0100

    _fix: Admin categories on listing edit.

--- resources/views/back/catalog/product/edit.blade.php
+++ resources/views/back/catalog/product/edit.blade.php
@@ -3,11 +3,7 @@
 @push('css_before')
     <link rel="stylesheet" href="{{ asset('assets/back/js/plugins/dropzone/min/dropzone.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/back/css/plugins/slim/slim.css') }}">
-
-
-        <link rel="stylesheet" href="{{ asset('assets/back/css/plugins/dataTables.bootstrap5.min.css') }}">
-
-
+    <link rel="stylesheet" href="{{ asset('assets/back/css/plugins/dataTables.bootstrap5.min.css') }}">
 
     @stack('gallery_css')
 
@@ -20,7 +16,6 @@
 @endpush
 
 @section('content')
-
     <div class="page-header">
         <div class="page-block">
             <div class="row align-items-center">
@@ -40,7 +35,6 @@
         </div>
     </div>
 
-
     <form action="{{ isset($product) ? route('product.update', ['product' => $product]) : route('product.store') }}" method="post" enctype="multipart/form-data">
         @csrf
         @if (isset($product))
@@ -49,69 +43,64 @@
         <div class="row">
             @include('back.layouts.partials.session')
             <div class="col-md-12">
-             <div class="card">
-                <div class="card-body py-0">
-                    <ul class="nav nav-tabs profile-tabs" id="myTab" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link active" id="general-tab" data-bs-toggle="tab" href="#general-tab-panel" role="tab"
-                               aria-selected="true">
-                                <i class="ti ti-info-circle me-2"></i>General
-                            </a>
-                        </li>
+                <div class="card">
+                    <div class="card-body py-0">
+                        <ul class="nav nav-tabs profile-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="general-tab" data-bs-toggle="tab" href="#general-tab-panel" role="tab"
+                                   aria-selected="true">
+                                    <i class="ti ti-info-circle me-2"></i>General
+                                </a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" id="time-tab" data-bs-toggle="tab" href="#time-tab-panel" role="tab"
-                               aria-selected="true">
-                                <i class="ti ti-clock me-2"></i>Working time
-                            </a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="time-tab" data-bs-toggle="tab" href="#time-tab-panel" role="tab"
+                                   aria-selected="true">
+                                    <i class="ti ti-clock me-2"></i>Working time
+                                </a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" id="menu-tab" data-bs-toggle="tab" href="#menu-tab-panel" role="tab"
-                               aria-selected="true">
-                                <i class="ti ti-file-certificate me-2"></i>Menu
-                            </a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="menu-tab" data-bs-toggle="tab" href="#menu-tab-panel" role="tab"
+                                   aria-selected="true">
+                                    <i class="ti ti-file-certificate me-2"></i>Menu
+                                </a>
+                            </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" id="images-tab" data-bs-toggle="tab" href="#images-tab-panel" role="tab"
-                               aria-selected="true">
-                                <i class="ti ti-photo me-2"></i>Images
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="seo-tab" data-bs-toggle="tab" href="#seo-tab-panel" role="tab"
-                               aria-selected="true">
-                                <i class="ti ti-settings me-2"></i>SEO
-                            </a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="images-tab" data-bs-toggle="tab" href="#images-tab-panel" role="tab"
+                                   aria-selected="true">
+                                    <i class="ti ti-photo me-2"></i>Images
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="seo-tab" data-bs-toggle="tab" href="#seo-tab-panel" role="tab"
+                                   aria-selected="true">
+                                    <i class="ti ti-settings me-2"></i>SEO
+                                </a>
+                            </li>
 
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-            </div>
             </div>
 
             <div class="tab-content">
-
                 <div class="tab-pane show active" id="general-tab-panel" role="tabpanel" aria-labelledby="general-tab">
 
                     <div class="col-md-12">
-                            <div class="card">
+                        <div class="card">
+                            <div class="d-flex card-header align-items-center justify-content-between">
+                                <h5 class="mb-0">General Info</h5>
 
-                                <div class="d-flex card-header align-items-center justify-content-between">
-                                    <h5 class="mb-0">General Info</h5>
-
-                                    <div class="form-check form-switch custom-switch-v1 ">
-                                        <input type="checkbox" class="form-check-input input-success" id="status-swich" name="status" @if (isset($product) and $product->status) checked @endif>
-                                        <label class="form-check-label" for="status-swich">Status</label>
-                                    </div>
-
+                                <div class="form-check form-switch custom-switch-v1 ">
+                                    <input type="checkbox" class="form-check-input input-success" id="status-swich" name="status" @if (isset($product) and $product->status) checked @endif>
+                                    <label class="form-check-label" for="status-swich">Status</label>
                                 </div>
+                            </div>
 
-
-
-                                <div class="card-body">
-                                    <div class="position-relative">
+                            <div class="card-body">
+                                <div class="position-relative">
                                     <ul class="nav nav-pills position-absolute langimg me-0 mb-2" id="pills-tab" role="tablist">
                                         @foreach(ag_lang() as $lang)
                                             <li class="nav-item">
@@ -142,12 +131,7 @@
                                             </div>
                                         @endforeach
                                     </div>
-                                    </div>
-
-
-
-
-
+                                </div>
                                 <div class="form-group row ">
                                     <div class="col-6 mt-3">
                                         <label for="address">Search address</label>
@@ -158,17 +142,13 @@
                                         </div>
                                     </div>
 
-
-
-
                                     <div class="col-6 mt-3">
                                         <label >Category</label>
                                         <select class="form-select" name="category">
                                             <option>Select...</option>
-                                            <option value="1"  {{ ((isset($product)) and ($product->category == '1')) ? 'selected' : '' }}>Bars</option>
-                                            <option value="2" {{ ((isset($product)) and ($product->category == '2')) ? 'selected' : '' }}>Clubs</option>
-                                            <option value="3"  {{ ((isset($product)) and ($product->category == '3')) ? 'selected' : '' }}>Food</option>
-                                            <option value="4"  {{ ((isset($product)) and ($product->category == '4')) ? 'selected' : '' }}>Fun</option>
+                                            @foreach (config('settings.categories') as $key => $category)
+                                                <option value="{{ $key }}" {{ ((isset($product)) and ($product->category == $key)) ? 'selected' : '' }}>{{ $category[current_locale()] }}</option>
+                                            @endforeach
                                         </select>
                                     </div>
                                 </div>
@@ -200,9 +180,6 @@
                                         <label for="city">Longitude</label>
                                         <input type="text" class="form-control" id="lon" name="lon" value="{{ isset($product) ? $product->lon : old('lon') }}" readonly/>
                                     </div>
-
-
-
                                 </div>
 
                             </div>
@@ -211,17 +188,9 @@
 
                     <div class="col-md-12">
                         <div class="card">
-
                             <h5 class="mb-0 card-header">Contact info</h5>
-
                             <div class="card-body">
-
-
-
-                                <div class="form-group row ">
-
-
-
+                                <div class="form-group row">
                                     <div class="col-md-6 mt-3">
 
                                         <div class="form-group">
@@ -396,7 +365,7 @@
                     </div>
 
                 </div>
-                </div>
+            </div>
 
 
             <div class="col-md-12">
@@ -472,8 +441,8 @@
                 document.getElementById("zip").value = itemPc;
                 document.getElementById("country").value = itemCountry;
 
-               document.getElementById('lat').value=lat;
-               document.getElementById('lon').value=lon;
+                document.getElementById('lat').value=lat;
+                document.getElementById('lon').value=lon;
 
                 //  document.getElementById('city').value = place.name;
                 if (!place.geometry) {
@@ -485,21 +454,16 @@
     </script>
 
     <script src="https://maps.googleapis.com/maps/api/js?key={{env('GOOGLE_API_KEY')}}&libraries=places&callback=initAutocomplete"></script>
+    
+    <script src="{{ asset('assets/back/js/plugins/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/back/js/plugins/dataTables.bootstrap5.min.js') }}"></script>
+    <script>
+        // [ Zero Configuration ] start
+        $('#simpletable').DataTable({
+            paging: false
+        });
 
-
-
-        <script src="{{ asset('assets/back/js/plugins/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('assets/back/js/plugins/dataTables.bootstrap5.min.js') }}"></script>
-        <script>
-            // [ Zero Configuration ] start
-            $('#simpletable').DataTable({
-                paging: false
-            });
-
-
-
-        </script>
-
+    </script>
 
     <script>
         $(() => {
