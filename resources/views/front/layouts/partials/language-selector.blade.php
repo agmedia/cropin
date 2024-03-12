42855d2c2111ccabde0848e658b822f455ed3b49
Filip Jankoski <filip@agmedia.hr>
2024-03-12 13:09:28 +0100

    _fix: Front lang selector.

--- resources/views/front/layouts/partials/language-selector.blade.php
+++ resources/views/front/layouts/partials/language-selector.blade.php
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 <div class="header-user-menu">
     <div class="header-user-name">
     <img class="lang" style="width:16px;" src="{{ asset('images/'.\Illuminate\Support\Str::upper(current_locale()).'.png') }}" alt=""> {{ \Illuminate\Support\Str::upper(current_locale()) }}
@@ -19,7 +13,22 @@
         @else
             @foreach (ag_lang() as $lang)
                 <li>
-                    <a class=" @if (current_locale() == $lang->code) active @endif" href="{{ LaravelLocalization::getLocalizedURL($lang->code, null, [], true) }}"><img class="lang" style="width:16px" src="{{ asset('images/'.Str::upper($lang->code).'.png') }}" alt=""> {{ $lang->title->{LaravelLocalization::getCurrentLocale()} }}</a>
+                    @if (isset($page))
+                        <a class=" @if (current_locale() == $lang->code) active @endif" href="{{ LaravelLocalization::getLocalizedURL($lang->code, $page->translation($lang->code)->slug, [], true) }}">
+                            <img class="lang" style="width:16px" src="{{ asset('images/'.Str::upper($lang->code).'.png') }}" alt="">
+                            {{ $lang->title->{LaravelLocalization::getCurrentLocale()} }}
+                        </a>
+                    @elseif(isset($product))
+                        <a class=" @if (current_locale() == $lang->code) active @endif" href="{{ LaravelLocalization::getLocalizedURL($lang->code, $product->translation($lang->code)->slug, [], true) }}">
+                            <img class="lang" style="width:16px" src="{{ asset('images/'.Str::upper($lang->code).'.png') }}" alt="">
+                            {{ $lang->title->{LaravelLocalization::getCurrentLocale()} }}
+                        </a>
+                    @else
+                        <a class=" @if (current_locale() == $lang->code) active @endif" href="{{ LaravelLocalization::getLocalizedURL($lang->code, null, [], true) }}">
+                            <img class="lang" style="width:16px" src="{{ asset('images/'.Str::upper($lang->code).'.png') }}" alt="">
+                            {{ $lang->title->{LaravelLocalization::getCurrentLocale()} }}
+                        </a>
+                    @endif
                 </li>
             @endforeach
         @endif
