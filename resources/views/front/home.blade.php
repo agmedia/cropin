47065b4f3aa24414e3ab0fabb775305b7037f765
tomashina <tomislav@agmedia.hr>
2024-03-14 11:13:06 +0100

    restrict-map

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -225,7 +225,16 @@
                     fullscreenControl: true,
                     navigationControl: false,
                     streetViewControl: false,
-                    animation: google.maps.Animation.BOUNCE,
+                    restriction: {
+                        latLngBounds: {
+                            north: 48,
+                            south: 40,
+                            east: 22,
+                            west: 11,
+                        },
+                    },
+
+
                     gestureHandling: 'greedy',
                     styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#4DB7FE"},{"visibility":"on"}]}]
                 });
