a8fd278af4417b42d102d83baf48b793add4754c
tomashina <tomislav@agmedia.hr>
2024-03-15 14:17:17 +0100

    domain

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-md-3">
                     <div class="about-widget">
-                        <img src="{{ asset('images/socializer.svg') }}" alt="Socializer">
+                        <img src="{{ asset('images/socializer-logo.svg') }}" alt="Socializer">
                     </div>
                 </div>
                 <div class="col-md-6">
