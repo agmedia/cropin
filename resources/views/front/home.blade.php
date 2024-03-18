ff7d22869b9f0aaf0c4785f396660e6db0e63cf9
tomashina <tomislav@agmedia.hr>
2024-03-18 15:51:24 +0100

    black

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -260,9 +260,7 @@
 
 
                     gestureHandling: 'greedy',
-                    styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#4DB7FE"},{"visibility":"on"}]}]
-
-
+                    styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#333"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#cccccc"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-50},{"lightness":15}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#4DB7FE"},{"visibility":"on"}]}]
                 });
 
                 var geolocationDiv = document.createElement('div');
@@ -484,7 +482,7 @@
                 }
 
                 var myCustomRegion = new google.maps.KmlLayer({
-                    url: 'https://www.socializertravel.com/images/vrt.kml',
+                    url: 'https://www.socializertravel.com/images/black.kml',
                     map: map,
                     suppressInfoWindows: true,
                     zindex: 0,
